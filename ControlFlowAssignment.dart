9609cd5159002ca0ef6d76e3bc04f7a68b74ad27
Agape77 <agapecamille@gmail.com>
2024-03-18 20:33:34 +0300

    spelling correction

--- ControlFlowAssignment.dart
+++ ControlFlowAssignment.dart
@@ -10,12 +10,13 @@ void main() {
   //Convert the input string into an integer
   int number = int.parse(input);
 
+  
   //Check the number against the criteria and print the message
   if (number > 10) {
     print("Your number is greater than 10");
   } else if (number < 10) {
     print("Your number is less than 10");
   } else {
-    print("Your number is equql to 10");
+    print("Your number is equal to 10");
   }
   }
